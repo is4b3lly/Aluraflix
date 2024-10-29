8839e94efb419d2f04a769d27e08bc67fe0bd889
is4b3lly <zaira.pauluk@gmail.com>
2024-10-29 09:18:42 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("toggle-button").addEventListener("click", function() {
+    const filmes = document.querySelector(".categoria-filmes");
+    if (filmes.style.display === "none") {
+        filmes.style.display = "flex";
+    } else {
+        filmes.style.display = "none";
+        }
+    });
